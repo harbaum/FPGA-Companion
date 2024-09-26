d116049676c82d189bca2beee6836ddfe2730ac8
vossstef <stefan.voss@live.de>
2024-09-26 16:47:13 +0200

    a2600 support

---
+++ src/core_atari2600.h
@@ -0,0 +1,17 @@
+//
+// core_atari2600.h
+//
+
+#ifndef CORE_ATARI2600_H
+#define CORE_ATARI2600_H
+
+#include <stdint.h>
+#include "menu.h"
+
+extern const char *core_atari2600_default_images[];
+extern const unsigned char core_atari2600_keymap[];
+extern const unsigned char core_atari2600_modifier[];
+extern const char *core_atari2600_forms[];
+extern menu_legacy_variable_t core_atari2600_variables[];
+
+#endif // CORE_atari2600_H
