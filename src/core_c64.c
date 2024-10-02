4eb39e3094416b2efbfb38b1144611cfdccf8298
Stefan Voss <stefan.voss@live.de>
2024-10-02 20:58:32 +0200

    c64 core osd

--- src/core_c64.c
+++ src/core_c64.c
@@ -32,7 +32,7 @@ static const char system_form_c64[] =
   // --------
   "L,Joyport 1:,Retro D9|USB #1 Joy|USB #2 Joy|NumPad|DualShock 2|Mouse|DS2 Paddle|USB #1 Padd|USB #2 Padd|Off,Q;"
   "L,Joyport 2:,Retro D9|USB #1 Joy|USB #2 Joy|NumPad|DualShock 2|Mouse|DS2 Paddle|USB #1 Padd|USB #2 Padd|Off,J;"
-  "L,Swap Joystick:,Off|On,&;"
+  "L,Swap Joyst:,Off|On,&;"
   "L,Invert Paddle:,Off|On,=;"
   "L,REU 1750:,Off|On,V;"
   "L,c1541 ROM:,Dolphin DOS|CBM DOS|Speed DOS P|Jiffy DOS,D;"
